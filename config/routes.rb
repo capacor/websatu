b02c4b76028845336c5e97cbba4e8c878beef0e1
capacor <nurizzatiqumillaila@gmail.com>
2024-01-20 21:50:29 +0700

    Bismillah2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'postingan/index'
+  get '/index' => 'home#index'
+  get '/artikel' => 'home#artikel'
+  get '/postingan' => 'postingan#index'
+  get '/about' => 'home#about'
+  get '/'=> 'home#index'
+  get '/postingan/detail/:id' => 'postingan#detail'
+  get '/postingan/input' => 'postingan#input'
+  post '/postingan/create' => 'postingan#create'
+
+  get '/postingan/edit/:id' => 'postingan#edit'
+  post '/postingan/update/:id' => 'postingan#update'
+  get '/postingan/delete/:id' => 'postingan#delete'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,5 +20,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  get "/home", to: "home#index"
+  root "home#index"
 end
